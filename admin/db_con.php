09f004b58f61d3a0e76b6dea3cd57b43c2edfeb1
SargentAli <2000356b@student.tp.edu.sg>
2022-11-24 08:49:27 +0000

    yes

--- admin/db_con.php
+++ admin/db_con.php
@@ -1,7 +1,8 @@
 <?php 
-define('DBHOST', 'localhost');
-define('DBUSER', 'root');
+define('DBHOST', 'glennserv.mysql.database.azure.com');
+define('DBUSER', 'glenn');
+define('DBPASS', 'P@ssw0rd');
 define('DBNAME', 'student');
 
 
-$db_con = mysqli_connect(DBHOST, DBUSER, '', DBNAME);
+$db_con = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
